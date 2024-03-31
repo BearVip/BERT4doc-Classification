ccd9f5a0b702ea1d574361e8f79f2748566820f8
BearVip <leehn.vip@foxmail.com>
2024-03-31 09:51:38 +0800

    Adjust on 20240331

--- codes/further-pre-training/00test.py
+++ codes/further-pre-training/00test.py
@@ -1,12 +1,18 @@
 import tensorflow as tf
 import numpy as np
+import pandas as pd
 import fastNLP as fn
-
+import sys
 import torch
-print(torch.__version__)
-print(torch.distributed.is_available())
 
 
-print(np.__version__)  
-print(tf.__version__)
-print(fn.__version__)
+
+print(torch.__version__) # 1.7.0+cpu
+print(torch.distributed.is_available()) # True
+
+print(pd.__version__) #　1.0.0
+print(np.__version__) # 1.16.0
+print(tf.__version__) # 1.15.0
+print(fn.__version__) # 1.0.1
+
+print(sys.version) # 3.7.9
